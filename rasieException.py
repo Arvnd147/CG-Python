43ff619d5e6a04da1d800a984c3b553bfa83259a
Aravind S <91778740+Arvnd147@users.noreply.github.com>
2023-07-17 20:41:24 +0530

    Add files via upload

---
+++ rasieException.py
@@ -0,0 +1,12 @@
+x = -1
+if x<0:
+    raise Exception()
+    #raise Exception("Sorry, no number below zero")
+    
+#another example
+"""
+x = "hello"
+
+if not type(x) is int:
+    raise TypeError("Only integers are allowed")
+"""
